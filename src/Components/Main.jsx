9f84ce322c38b05698d664fad8124f8298c1bddc
rajibdas-it <rajibrad@gmail.com>
2024-08-25 21:55:09 +0600

    html template setup done

---
+++ src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import MovieList from "./MovieList";
+import Sidebar from "./Sidebar";
+
+const Main = () => {
+  return (
+    <main>
+      <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <Sidebar />
+        <MovieList />
+      </div>
+    </main>
+  );
+};
+
+export default Main;
