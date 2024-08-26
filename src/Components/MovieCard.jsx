8bc38b9f4f1446d65e10e02c8e52cfeaba7bc357
rajib <rajibrad@gmail.com>
2024-08-26 14:34:30 +0600

    fix tailwind config file

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -35,7 +35,7 @@ const MovieCard = ({ movie }) => {
               <Rating ratingValue={movie.rating} />
             </div>
             <a
-              className="bg-[#] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+              className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
               href="#"
             >
               <img src="./assets/tag.svg" alt="" />
