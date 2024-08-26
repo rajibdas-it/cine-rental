95bf21382aeaf1fcb02d9bac71e49fd5ffaeb5a9
rajib <rajibrad@gmail.com>
2024-08-26 14:46:50 +0600

    add cart template

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,10 +3,23 @@ import Ring from "../assets/ring.svg";
 // import Sun from "../assets/icons/sun.svg";
 import Moon from "../assets/icons/moon.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
+import { useState } from "react";
+import CartDetails from "./CartDetails";
 
 const Header = () => {
+  const [showCart, setShowCart] = useState(false);
+
+  const handleCart = () => {
+    setShowCart(true);
+  };
+
+  const handleCartClose = () => {
+    setShowCart(false);
+  };
+
   return (
     <header>
+      {showCart && <CartDetails onCartClose={handleCartClose} />}
       <nav className="container flex items-center justify-between space-x-10 py-6">
         <a>
           <img src={Logo} width="139" height="26" alt="" />
@@ -32,7 +45,7 @@ const Header = () => {
           <li>
             <a
               className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
+              onClick={handleCart}
             >
               <img src={ShoppingCart} width="24" height="24" alt="" />
             </a>
